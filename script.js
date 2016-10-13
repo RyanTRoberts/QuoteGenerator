ec05124e428ad7447dc4eb41815273877217068d
RyanTRoberts <ryan.trevor.roberts@gmail.com>
2016-10-13 16:27:07 +1000

    required changes
    should no longer display <span>$ for the year or citation class if they
    are not needed, though i am still trying to find a way to prevent the
    same quote being generated twice in a row

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//quotes array
 var quotes = [
     {
         quote: "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
@@ -20,24 +21,60 @@ var quotes = [
     },
     {   
         quote: "Happiness is when what you think, what you say, and what you do are in harmony.",
-        source: "Mahatma Gandhi"
+        source: "Chip Conley",
+        citation:"Ted Talk: Measuring what makes life worthwhile",
+        year:"2010"
+        
     },
     {   
-        quote: "In order to carry a positive action we must develop here a position vision.",
+        quote: "What one thing can you start counting today that will actually be meaningful in your life?",
         source: "Dalai Lama"
     }    
 ];
 
+//Return a randomly selected a quote from the quotes array
 function getRandomQuote() {
+    
     var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    
     return randomQuote;
 }
 
+
+
+
+
+
+
 function printQuote() {
-    var randomQuote = getRandomQuote();
-    document.getElementById('quote-box').innerHTML = ('<p class="quote"> ' + randomQuote.quote + '</p> <p class="source">' + randomQuote.source + '<span class="citation"> </span> <span class="year"> </span> </p>');
     
+    var randomQuote = getRandomQuote();
+    
+//If object contains year & citation properties
+if(randomQuote.hasOwnProperty('citation') == true && randomQuote.hasOwnProperty('year') == true) {
+        
+    //Create <span>$ for the properties
+        document.getElementById('quote-box').innerHTML = ('<p class="quote"> ' + randomQuote.quote + '</p> <p class="source">' + randomQuote.source + '<span class="citation">' + randomQuote.citation + '</span> <span class="year">' + randomQuote.year + '</span> </p>');
+    
+    //else If object contains year property
+    } else if(randomQuote.hasOwnProperty('year') == true) {
+       
+        //Create <span> for citation property
+        document.getElementById('quote-box').innerHTML = ('<p class="quote"> ' + randomQuote.quote + '</p> <p class="source">' + randomQuote.source + '<span class="year">' + randomQuote.year + '</span></p>');
+    
+    //else create spans for quote and source properties only
+    } else if(randomQuote.hasOwnProperty('citation') == true){
+       
+        document.getElementById('quote-box').innerHTML = ('<p class="quote"> ' + randomQuote.quote + '</p> <p class="source">' + randomQuote.source + '<span class="citation">' + randomQuote.citation + '</span></p>');  
+    
+    } else {
+        document.getElementById('quote-box').innerHTML = ('<p class="quote"> ' + randomQuote.quote + '</p> <p class="source">' + randomQuote.source + '</p>');
+    }
 }
+
+
+
+
 
 
 
